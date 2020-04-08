a6b6025fdea777a6fd55612212edec4944af5ecb
wintoro <wintorobudipuput@darmajaya.ac.id>
2020-04-08 15:11:50 +0700

    30-connecting functions and button

--- serction2/lib/main.dart
+++ serction2/lib/main.dart
@@ -7,14 +7,44 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion(){
+    print('answer choosen');
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    var question = [
+      'what\'s your name',
+      'what\'s your favourite color',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My FIrst App'),
         ),
-        body: Text('This is Default Text'),
+        body: Column(
+          children: [
+            Text('what is your name?'),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer4'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
