3c7a12d9276496dc8f9e77fad3c7659b88d3f924
wintoro <wintorobudipuput@darmajaya.ac.id>
2020-04-08 14:07:55 +0700

    building a widget tree

--- serction2/lib/main.dart
+++ serction2/lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
+// void main(){
+//   runApp(MyApp());
+// }
 
-void main(){
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My FIrst App'),
+        ),
+        body: Text('This is Default Text'),
+      ),
+    );
+  }
 }
-
- class MyApp extends StatelessWidget{ 
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello'),);
-   }
- }
-
- 
